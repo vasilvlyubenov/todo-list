546f92349e2cd4d4c5faa35de9ba29d15d091fa5
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-24 18:52:52 +0300

    project initialization and added project structure

---
+++ todo-list-app/src/app/task/task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+    tasks = ['task 1', 'task 2'];
+}
